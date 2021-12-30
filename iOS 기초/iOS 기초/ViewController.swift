05eb470abc5dbdb02ff20cc73d4f20f0f6bc48b5
Kim <xodnjs0876@gmail.com>
2021-12-31 01:04:56 +0900

    기초 코드

---
+++ iOS 기초/iOS 기초/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS 기초
+//
+//  Created by 김태원 on 2021/12/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
