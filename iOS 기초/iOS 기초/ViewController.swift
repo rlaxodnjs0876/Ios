8233e703c0c8ec767ae007ae8b3a1d96abe308b4
Kim <xodnjs0876@gmail.com>
2022-01-01 20:53:56 +0900

    기본 틀 작성

--- iOS 기초/iOS 기초/ViewController.swift
+++ iOS 기초/iOS 기초/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var PlayPauseButton:UIButton!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var progressSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
+        print("button paused")
+    }
+    
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print("slider Value Changed")
+    }
 }
 
