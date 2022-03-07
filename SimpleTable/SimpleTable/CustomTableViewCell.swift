6015b708a452707ada75e5c213a8174457b94db3
Kim <xodnjs0876@gmail.com>
2022-03-07 20:12:31 +0900

    TableView

---
+++ SimpleTable/SimpleTable/CustomTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustomTableViewCell.swift
+//  SimpleTable
+//
+//  Created by 김태원 on 2022/02/04.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet var leftLabel : UILabel!
+    @IBOutlet var rightLabel : UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SimpleTable/SimpleTable/SecondViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SecondViewController.swift
+//  SimpleTable
+//
+//  Created by 김태원 on 2022/02/05.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var textToSet:String?
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.textLabel.text = self.textToSet
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
