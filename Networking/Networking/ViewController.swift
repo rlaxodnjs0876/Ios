418356d5549fd81b04bc20a973da243aa2663403
Kim <xodnjs0876@gmail.com>
2022-03-08 19:54:46 +0900

    데이터 요청

--- Networking/Networking/ViewController.swift
+++ Networking/Networking/ViewController.swift
@@ -7,13 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource{
 
+    @IBOutlet weak var tableView: UITableView!
+    let cellIdentifier:String = "friendCell"
+    var friends: [Friend] = []
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        
+        let friend: Friend = self.friends[indexPath.row]
+        
+        cell.textLabel?.text = friend.name.full
+        cell.detailTextLabel?.text = friend.email
+        
+        guard let imageURL: URL = URL(string: friend.picture.thumbnail) else { return cell }
+        guard let imageData: Data =  try? Data(contentsOf: imageURL) else { return cell }
+        cell.imageView?.image = UIImage(data: imageData)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friends.count
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        guard let url:URL = URL(string: "https://randomuser.me/api/?results=20&inc=name,email,picture") else {return}
+        
+        let session: URLSession = URLSession(configuration: .default)
+        let dataTask: URLSessionDataTask = session.dataTask(with: url) {
+            (data: Data?, response: URLResponse?, error:Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let data = data else {return}
+            
+            do{
+                let apiResponse: APIResponse = try JSONDecoder().decode(APIResponse.self, from: data)
+                self.friends = apiResponse.results
+                self.tableView.reloadData()
+            } catch(let err) {
+                print(err.localizedDescription)
+            }
+        }
+        
+        dataTask.resume()
+    }
 
 }
 
